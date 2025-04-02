b132db5f50cc9345a3a700c6854c23a012d888c0
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-04-02 00:05:44 -0300

    criar classe controller 'AnimacaoLoginCadastro'
    - criar nova classe em controllers para a animação a foto na tela de login
    - passar o código de Form1 para nova classe

---
+++ Controllers/AnimacaoLogin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPI.Controllers
+{
+    class AnimacaoLogin
+    {
+        public async Task TesteAnimacao(int targetX, PictureBox pictureBox)
+        {
+            int duracaoMs = 200; // Duração da animação
+            int framesPorSegundo = 120; //FPS
+            int delay = 1000 / framesPorSegundo; //Calculo para manter o FPS
+            int inicialX = pictureBox.Location.X; //Posição inicial
+            int distancia = targetX - inicialX; //Distancia entre inicial e final
+            int framesTotais = (int)Math.Ceiling(duracaoMs / (1000.0 / framesPorSegundo)); //Quantos frames
+
+            for (int frame = 0; frame <= framesTotais; frame++)
+            {
+                float progresso = (float)frame / framesTotais; //Progreção da animação
+                float raizProgresso = (float)Math.Pow(progresso, 0.5); //"Suavizar" animação 
+                int atualX = inicialX + (int)(distancia * raizProgresso); //Posição durante a animação
+
+                pictureBox.Location = new Point(atualX, pictureBox.Location.Y); //Movimenta a imagem
+                await Task.Delay(delay);
+            }
+
+            pictureBox.Location = new Point(targetX, pictureBox.Location.Y); //Garante que ela fique na posição final
+
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoPI.Controllers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,39 +19,14 @@ namespace ProjetoPI
             InitializeComponent();
         }
 
-
-
-        private async Task testeAnimacao(int targetX)
-        {
-            int durationMs = 200; // Duração da animação
-            int framesPerSecond = 120; //FPS
-            int delay = 1000 / framesPerSecond; //Calculo para manter o FPS
-            int initialX = pictureBox1.Location.X; //Posição inicial
-            int distance = targetX - initialX; //Distancia entre inicial e final
-            int totalFrames = (int)Math.Ceiling(durationMs / (1000.0 / framesPerSecond)); //Quantos frames
-
-            for (int frame = 0; frame <= totalFrames; frame++)
-            {
-                float progress = (float)frame / totalFrames; //Progreção da animação
-                float easedProgress = (float)Math.Pow(progress, 0.5); //"Suavizar" animação 
-                int currentX = initialX + (int)(distance * easedProgress); //Posição durante a animação
-
-                pictureBox1.Location = new Point(currentX, pictureBox1.Location.Y); //Movimenta a imagem
-                await Task.Delay(delay);
-            }
-
-            pictureBox1.Location = new Point(targetX, pictureBox1.Location.Y); //Garante que ela fique na posição final
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            testeAnimacao(tableLayoutPanel1.Width - pictureBox1.Width+12);
+            new AnimacaoLogin().TesteAnimacao(tableLayoutPanel1.Width - pictureBox1.Width+12, pictureBox1);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            testeAnimacao(12);
+            new AnimacaoLogin().TesteAnimacao(12, pictureBox1);
         }
 
         private void Form1_Resize(object sender, EventArgs e)
