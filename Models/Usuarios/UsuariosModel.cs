b248d54b0254273a9c5943d17b273725e4eda020
lilianlacerda <lilianraiany@gmail.com>
2025-04-11 20:38:15 -0300

    Incluir classe usuarios Model com Get e Set. Também com um método para tranformar os dados do banco em objeto usuario

---
+++ Models/Usuarios/UsuariosModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ProjetoPI.Models.Usuarios
+{
+    public class UsuariosModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string login { get; set; }
+        public string SenhaHash { get; set; }
+        public DateTime dataCriacao { get; set; }
+
+        public static UsuariosModel UserFromDataReader(MySqlDataReader reader)
+        {
+
+            return new UsuariosModel
+            {
+                Id = Convert.ToInt32(reader["Id"]),
+                Nome =  Convert.ToString(reader["Nome"]),
+                login = Convert.ToString(reader["login"]),
+                SenhaHash = Convert.ToString(reader["SenhaHash"]),
+                dataCriacao = Convert.ToDateTime(reader["dataCriacao"])
+            };
+        }
+    }
+}
