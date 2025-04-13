8e9e4b5fc8d1e04ab783c9c498769706c4a01f14
lilianlacerda <lilianraiany@gmail.com>
2025-04-13 16:03:01 -0300

    Adicionar a classe de sessão do usuário. Para identificar qual usuário está logado e quando ele for deslogado.

---
+++ Services/SessaoUsuario.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoPI.Models.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPI.Services
+{
+
+    // Classe para gerenciar a sessão do usuário logado. É staticamente acessível em toda a aplicação
+    public static class SessaoUsuario
+    {
+        // Variável estática que guarda o usuário que está atualmente logado no sistema.
+        // Como é estática, seu valor é compartilhado e único em toda a aplicação (enquanto o app estiver aberto).
+        public static Usuarios usuarioLogado;
+
+        // Método para verificar se o usuário está logado
+        public static Usuarios Login(Usuarios usuario)
+        {
+            return usuarioLogado = usuario;
+        }
+
+        //Esse método "desloga" o usuário, limpando a variável usuarioLogado.
+        public static void Logout()
+        {
+            usuarioLogado = null;
+        }
+    }
+}
