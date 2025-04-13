f0d0112377eb7627d107dee8547901679b9ad9b2
lilianlacerda <lilianraiany@gmail.com>
2025-04-13 15:01:02 -0300

    Inserir query para cadastramento de usuários.

--- Models/Usuarios/UsuariosRepository.cs
+++ Models/Usuarios/UsuariosRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProjetoPI.Services;
+using ProjetoPI.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,37 @@ using System.Threading.Tasks;
 
 namespace ProjetoPI.Models.Usuarios
 {
-    class UsuariosRepository
+    public class UsuariosRepository
     {
+        DataBaseService _databaseService;
+
+        public UsuariosRepository(DataBaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
+
+        public bool RegistrarUsuarios(Usuarios usuario, string senha)
+        {
+            try
+            {
+                string query = @"INSERT INTO usuarios (nome, login, senhaHash, createdAt) VALUES(@nome, @login, @senhaHash, @createdAt)";
+
+                MySqlParameter[] parameters = new MySqlParameter[]
+                {
+                    new MySqlParameter("@nome", usuario.Nome),
+                    new MySqlParameter("@login", usuario.login),
+                    new MySqlParameter("@senhaHash", senha),
+                    new MySqlParameter("@createdAt", DateTime.Now)
+                };
+
+                int affectedRows = _databaseService.ExecuteNonQuery(query, parameters);
+                return affectedRows > 0;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao registrar usuário: " + ex.Message);
+            }
+        }
+
     }
 }
