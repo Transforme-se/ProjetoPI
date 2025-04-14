22bf18cbd4f13a96b2bad2c30c71bd197dd5392e
lilianlacerda <lilianraiany@gmail.com>
2025-04-14 19:58:32 -0300

    criar classe MetasRepository criando a query para listar todas as metas.

---
+++ Models/Metas/MetasRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using ProjetoPI.Services;
+
+namespace ProjetoPI.Models.Metas
+{
+    class MetasRepository
+    {
+        DataBaseService _databaseService;
+
+        public MetasRepository(DataBaseService databaseService)
+        {
+            _databaseService = databaseService;
+        }
+
+        public List<Metas> ObterTodasMetas()
+        {
+            List<Metas> metas = new List<Metas>();
+
+            try
+            {
+                string query = "SELECT * FROM metas";
+                MySqlDataReader resultadoBanco = _databaseService.ExecuteQuery(query);
+
+                while (resultadoBanco.Read())
+                {
+                    Metas meta = new Metas();
+                    meta = Metas.UserFromDataReade(resultadoBanco);
+
+                    metas.Add(meta);
+                }
+                _databaseService.CloseConnection();
+
+                return metas;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao buscar metas: " + ex.Message);
+            }
+        }
+        
+
+    }
+}
