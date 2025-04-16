48003321808c29e1413ab23715cec3f0f619c17d
lilianlacerda <lilianraiany@gmail.com>
2025-04-15 21:25:24 -0300

    Criação de tela inicial de teste.

---
+++ Views/TelaInicio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPI.Views
+{
+    public partial class TelaInicio: Form
+    {
+        public TelaInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
