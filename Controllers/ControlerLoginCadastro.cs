0824382ac96c0118b6dfea39d4cc33b71d2b567b
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-04-16 18:55:28 -0300

    criar controller para login

---
+++ Controllers/ControlerLoginCadastro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjetoPI.Services;
+
+namespace ProjetoPI.Controllers
+{
+    class ControlerLoginCadastro
+    {
+        public bool Login (string login, string senha)
+        {
+            AutenticacaoService autenticacaoService = new AutenticacaoService(new DataBaseService());
+            var usuario = autenticacaoService.Autenticacao(login, senha);
+            if (usuario == null)
+            {
+                MessageBox.Show("Login ou senha inválidos");
+                return false;
+            }
+            return true;
+        }
+    }
+}
