1ab7085f8cefec150a8a771f08480a458c52841a
lilianlacerda <lilianraiany@gmail.com>
2025-05-06 21:11:01 -0300

    Add: atributos e adição no construtor de foto de perfil e foto de fundo

--- Models/Usuarios/Usuarios.cs
+++ Models/Usuarios/Usuarios.cs
@@ -13,6 +13,8 @@ namespace ProjetoPI.Models.Usuarios
         public string Nome { get; set; }
         public string login { get; set; }
         public string SenhaHash { get; set; }
+        public int FotoFundo { get; set; }
+        public int FotoPerfil { get; set; }
 
         // Contrutor para pegar os dados que vieram do banco de dados e transformar em um objeto Usuarios
         public static Usuarios UserFromDataReader(MySqlDataReader reader)
@@ -23,7 +25,9 @@ namespace ProjetoPI.Models.Usuarios
                 Id = Convert.ToInt32(reader["IdUsuarios"]),
                 Nome =  Convert.ToString(reader["Nome"]),
                 login = Convert.ToString(reader["Login"]),
-                SenhaHash = Convert.ToString(reader["SenhaHash"])
+                SenhaHash = Convert.ToString(reader["SenhaHash"]),
+                FotoFundo = Convert.ToInt32(reader["FotoFundo"]),
+                FotoPerfil = Convert.ToInt32(reader["FotoPerfil"])
             };
         }
     }
