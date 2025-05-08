eefe058304f8b2f44fa375f9aefde7ff072f03db
lilianlacerda <lilianraiany@gmail.com>
2025-05-08 19:38:06 -0300

    add: classe de mensagens

---
+++ Models/Mensagens/Mensagens.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ProjetoPI.Models.Mensagens
+{
+    public class Mensagens
+    {
+        public int IdMensagens { get; set; }
+        public String Mensagem { get; set; }
+
+        public static Mensagens UserFromDataReader(MySqlDataReader reader)
+        {
+            return new Mensagens
+            {
+                IdMensagens = Convert.ToInt32(reader["IdMensagens"]),
+                Mensagem = Convert.ToString(reader["Mensagem"])
+            };
+        }
+    }
+}
