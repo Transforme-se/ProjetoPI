66897e0d230c3a1cc4b09e089325ab7f10f3904a
lilianlacerda <lilianraiany@gmail.com>
2025-05-08 19:38:35 -0300

    Add: Repository de mensagens com a query para mostrar uma mensagem

---
+++ Models/Mensagens/MensagensRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using ProjetoPI.Services;
+
+namespace ProjetoPI.Models.Mensagens
+{
+    class MensagensRepository
+    {
+        DataBaseService _dataBaseService;
+
+        public MensagensRepository(DataBaseService dataBaseService)
+        {
+            _dataBaseService = dataBaseService;
+        }
+
+        public Mensagens ObterMensagem(int id ) 
+        {
+            string query = "SELECT mensagem FROM mensagens WHERE idMensagens = @id";
+            MySqlParameter[] parameters = new MySqlParameter[]
+            {
+                new MySqlParameter("@id", id)
+            };
+
+            using (MySqlDataReader resultadoBanco = _dataBaseService.ExecuteQuery(query, parameters))
+            {
+                if (resultadoBanco.Read())
+                {
+                    Mensagens mensagem = Mensagens.UserFromDataReader(resultadoBanco);
+                    return mensagem;
+                }
+            }
+
+            return null;
+        }
+    }
+}
