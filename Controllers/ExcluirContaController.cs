4481cb37576f034199a635acff546fe2aa0b2ec4
lilianlacerda <lilianraiany@gmail.com>
2025-05-10 14:33:42 -0300

    adicionar controller do método de excluir conta por completo.

---
+++ Controllers/ExcluirContaController.cs
@@ -0,0 +1,37 @@
+﻿using ProjetoPI.Models.Usuarios;
+using ProjetoPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPI.Controllers
+{
+    public class ExcluirContaController
+    {
+        private readonly DataBaseService _databaseService;
+        private readonly UsuariosRepository _usuariosRepository;
+
+        public ExcluirContaController(DataBaseService databaseService)
+        {
+            _databaseService = databaseService;
+            _usuariosRepository = new UsuariosRepository(_databaseService);
+        }
+
+        public bool ExcluirConta(int idUsuario)
+        {
+            try
+            {
+
+                return _usuariosRepository.ExcluirUsuario(idUsuario);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao excluir conta: " + ex.Message);
+            }
+        }
+
+    }
+}
