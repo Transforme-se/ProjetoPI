ea310ba22c43fa0cb2ec794e9eab3a6017c70812
lilianlacerda <lilianraiany@gmail.com>
2025-05-11 18:02:39 -0300

    correção no corte da mensagem motivacional

--- Views/MostrarMensagem.Designer.cs
+++ Views/MostrarMensagem.Designer.cs
@@ -29,54 +29,56 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MostrarMensagem));
-            this.lblMensagem = new Krypton.Toolkit.KryptonLabel();
             this.kryptonPictureBox1 = new Krypton.Toolkit.KryptonPictureBox();
+            this.txtMensagem = new Krypton.Toolkit.KryptonRichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lblMensagem
-            // 
-            this.lblMensagem.Location = new System.Drawing.Point(91, 24);
-            this.lblMensagem.Name = "lblMensagem";
-            this.lblMensagem.Size = new System.Drawing.Size(96, 28);
-            this.lblMensagem.StateCommon.LongText.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
-            this.lblMensagem.StateCommon.ShortText.Font = new System.Drawing.Font("Corbel Light", 14.25F, System.Drawing.FontStyle.Italic);
-            this.lblMensagem.TabIndex = 0;
-            this.lblMensagem.Values.Text = "Mensagem";
-            this.lblMensagem.Click += new System.EventHandler(this.lblMensagem_Click);
             // 
             // kryptonPictureBox1
             // 
             this.kryptonPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("kryptonPictureBox1.Image")));
-            this.kryptonPictureBox1.Location = new System.Drawing.Point(73, 77);
+            this.kryptonPictureBox1.Location = new System.Drawing.Point(78, 100);
+            this.kryptonPictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.kryptonPictureBox1.Name = "kryptonPictureBox1";
-            this.kryptonPictureBox1.Size = new System.Drawing.Size(140, 78);
+            this.kryptonPictureBox1.Size = new System.Drawing.Size(187, 96);
             this.kryptonPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.kryptonPictureBox1.TabIndex = 1;
             this.kryptonPictureBox1.TabStop = false;
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMensagem.Location = new System.Drawing.Point(10, 10);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.ReadOnly = true;
+            this.txtMensagem.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.txtMensagem.Size = new System.Drawing.Size(366, 83);
+            this.txtMensagem.StateCommon.Border.Draw = Krypton.Toolkit.InheritBool.False;
+            this.txtMensagem.StateCommon.Content.Font = new System.Drawing.Font("Corbel Light", 14.25F, System.Drawing.FontStyle.Italic);
+            this.txtMensagem.Text = "Mensagem";
+            this.txtMensagem.TabStop = false;
             // 
             // MostrarMensagem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(291, 188);
+            this.ClientSize = new System.Drawing.Size(388, 231);
             this.ControlBox = false;
+            this.Controls.Add(this.txtMensagem);
             this.Controls.Add(this.kryptonPictureBox1);
-            this.Controls.Add(this.lblMensagem);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MostrarMensagem";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mensagem";
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private Krypton.Toolkit.KryptonLabel lblMensagem;
         private Krypton.Toolkit.KryptonPictureBox kryptonPictureBox1;
+        private Krypton.Toolkit.KryptonRichTextBox txtMensagem;
     }
 }
